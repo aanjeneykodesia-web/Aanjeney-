181c1418778ba7b6ca585f9e2086927964ae99c2
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-21 09:51:52 +0530

    Add ManufacturerLogin widget for login screen

---
+++ lib/Manufacturer_login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ManufacturerLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Manufacturer Login")),
+      body: const Center(child: Text("Manufacturer Dashboard")),
+    );
+  }
+}
