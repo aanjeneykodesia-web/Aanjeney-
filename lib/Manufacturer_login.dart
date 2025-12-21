ac50b83f098ebb9d5a1507dd9ce11219cce63200
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-21 12:28:14 +0530

    Update Manufacturer login page titles

--- lib/Manufacturer_login.dart
+++ lib/Manufacturer_login.dart
@@ -4,8 +4,8 @@ class ManufacturerLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Manufacturer Login")),
-      body: const Center(child: Text("Manufacturer Dashboard")),
+      appBar: AppBar(title: const Text("Manufacturer")),
+      body: const Center(child: Text("Manufacturer Page")),
     );
   }
 }
