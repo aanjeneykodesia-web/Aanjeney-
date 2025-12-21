241a8818be957be6930078e85630466d12af93e5
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-21 09:44:57 +0530

    Add ShopkeeperLogin widget for shopkeeper dashboard

---
+++ lib/Shopkeeper_login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ShopkeeperLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Shopkeeper Login")),
+      body: const Center(child: Text("Shopkeeper Dashboard")),
+    );
+  }
+}
