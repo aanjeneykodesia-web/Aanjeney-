14ec93eada59d3e2388753c7d72f498dffddb560
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-21 12:52:37 +0530

    Add TransporterLogin widget with basic layout

---
+++ lib/transporter_login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TransporterLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Transporter")),
+      body: const Center(child: Text("Transporter Page")),
+    );
+  }
+}
