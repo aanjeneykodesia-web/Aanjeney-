855ac9f4f3da96101731e2acb9661966bbdd1197
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-21 12:19:31 +0530

    Update titles in Shopkeeper login UI

--- lib/Shopkeeper_login.dart
+++ lib/Shopkeeper_login.dart
@@ -4,8 +4,8 @@ class ShopkeeperLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Shopkeeper Login")),
-      body: const Center(child: Text("Shopkeeper Dashboard")),
+      appBar: AppBar(title: const Text("Shopkeeper")),
+      body: const Center(child: Text("Shopkeeper Page")),
     );
   }
 }
