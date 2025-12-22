097aa4ef8d8f95d60c9f63a4f802b15eb6319d61
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-22 22:08:25 +0530

    Refactor AdminDashboard to use StatelessWidget
    Refactored AdminDashboard from StatefulWidget to StatelessWidget, removing product management functionality and updating the UI to display static text.

--- lib/admin_dashboard.dart
+++ lib/admin_dashboard.dart
@@ -1,51 +1,19 @@
 import 'package:flutter/material.dart';
-import 'product_model.dart';
 
-class AdminDashboard extends StatefulWidget {
-  @override
-  State<AdminDashboard> createState() => _AdminDashboardState();
-}
-
-class _AdminDashboardState extends State<AdminDashboard> {
-  final List<Product> products = [];
-  final nameCtrl = TextEditingController();
-  final qtyCtrl = TextEditingController();
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Admin Dashboard")),
-      body: Column(
-        children: [
-          TextField(
-            controller: nameCtrl,
-            decoration: const InputDecoration(labelText: "Product Name"),
-          ),
-          TextField(
-            controller: qtyCtrl,
-            decoration: const InputDecoration(labelText: "Quantity"),
-            keyboardType: TextInputType.number,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                products.add(
-                  Product(nameCtrl.text, int.parse(qtyCtrl.text)),
-                );
-              });
-            },
-            child: const Text("Add Product"),
-          ),
-          Expanded(
-            child: ListView(
-              children: products
-                  .map((p) => ListTile(
-                        title: Text(p.name),
-                        trailing: Text(p.quantity.toString()),
-                      ))
-                  .toList(),
-            ),
-          ),
+      body: ListView(
+        padding: const EdgeInsets.all(12),
+        children: const [
+          Text("All Shopkeepers"),
+          Text("All Manufacturers"),
+          Text("All Transporters"),
+          Text("All Products"),
         ],
       ),
     );
