48e06dd550eab301b860e3a11e46635818dfedcf
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-23 07:38:12 +0530

    Refactor ShopkeeperDashboard to StatelessWidget
    Refactor ShopkeeperDashboard to use StatelessWidget and simplify UI.

--- lib/shopkeeper_dashboard.dart
+++ lib/shopkeeper_dashboard.dart
@@ -1,47 +1,12 @@
 import 'package:flutter/material.dart';
-import '../models/product_model.dart';
 
-class ShopkeeperDashboard extends StatefulWidget {
-  const ShopkeeperDashboard({super.key});
-
-  @override
-  State<ShopkeeperDashboard> createState() => _ShopkeeperDashboardState();
-}
-
-class _ShopkeeperDashboardState extends State<ShopkeeperDashboard> {
-  List<Product> products = [];
-
-  final nameCtrl = TextEditingController();
-  final priceCtrl = TextEditingController();
-  final qtyCtrl = TextEditingController();
-
+class ShopkeeperDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Shopkeeper Dashboard")),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          children: [
-            TextField(controller: nameCtrl, decoration: const InputDecoration(labelText: "Product Name")),
-            TextField(controller: priceCtrl, decoration: const InputDecoration(labelText: "Price")),
-            TextField(controller: qtyCtrl, decoration: const InputDecoration(labelText: "Quantity")),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  products.add(Product(
-                    name: nameCtrl.text,
-                    price: double.parse(priceCtrl.text),
-                    quantity: int.parse(qtyCtrl.text),
-                  ));
-                });
-              },
-              child: const Text("Add Product"),
-            ),
-            const Divider(),
-            const Text("📍 Location sent to Manufacturer"),
-          ],
-        ),
+      body: const Center(
+        child: Text("Place Orders & Edit Products"),
       ),
     );
   }
