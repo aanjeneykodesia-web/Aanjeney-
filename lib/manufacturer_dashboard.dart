5c6658601dc8632a98e8f6d45cfbc0694b02f91c
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-23 07:39:06 +0530

    Update manufacturer_dashboard.dart

--- lib/manufacturer_dashboard.dart
+++ lib/manufacturer_dashboard.dart
@@ -1,22 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ManufacturerDashboard extends StatelessWidget {
-  const ManufacturerDashboard({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Manufacturer Dashboard")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text("Shopkeeper Name: Demo Shop"),
-            Text("Mobile: 9XXXXXXXXX"),
-            SizedBox(height: 10),
-            Text("Approve & send manufacturing location"),
-          ],
-        ),
+      body: const Center(
+        child: Text("Approve Shopkeeper Orders"),
       ),
     );
   }
